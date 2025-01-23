3393650bbe08d8d8f0c940a990a7451b6684f18f
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-23 13:51:40 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -5,12 +5,20 @@ function checkAnswer() {
     const correctAnswer = "4";
     let userAnswer = document.querySelector('input[name="quiz"]:checked');
 
-   f (userAnswer.value === correctAnswer){
+    if (userAnswer.value){
+
+        if (userAnswer.value == correctAnswer) {
             feedback.textContent = "Correct! Well done.";
         }
         else {
             feedback.textContent = "That's incorrect. Try again!";
         }
+    }
+        
+    else {
+        feedback.textContent="please choose something";
+    }
+
 }
 
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
