d60f5e4ae0775840cde61757f38ae38461ee5c7d
23Siya <156370624+23Siya@users.noreply.github.com>
2025-01-23 13:45:54 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -5,17 +5,13 @@ function checkAnswer() {
     const correctAnswer = "4";
     let userAnswer = document.querySelector('input[name="quiz"]:checked');
 
-    if (userAnswer) {
+   
         if (userAnswer.value == correctAnswer) {
             feedback.textContent = "Correct! Well done.";
         }
         else {
             feedback.textContent = "That's incorrect. Try again!";
         }
-    }
-    else {
-        feedback.textContent="please choose something";
-    }
 }
 
 document.getElementById('submit-answer').addEventListener('click', checkAnswer);
